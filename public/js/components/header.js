f685d9d2e1294dd9475317ce0860cf2587368a57
Betsi Loayza <betsi18_12@hotmail.com>
2017-06-20 15:29:51 -0500

    diseño primera pantalla html

---
+++ public/js/components/header.js
@@ -0,0 +1,10 @@
+// 'use strict';
+//
+// const Header = (update) => {
+//   const header = $("<header></header>");
+//   const title = $("<span>Prueba</span>");
+//
+//   header.append(title);
+//
+//   return header;
+// }
---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+// 'use strict';
+ $('.carousel.carousel-slider').carousel({fullWidth: true});
+// const render = (root) => {
+//   root.empty();
+//   const wrapper = $('<div class="wrapper"></div>');
+//   wrapper.append(Header(_ => render(root)));
+//
+//   root.append(wrapper);
+// }
+//
+// const state = {
+//   usuarios: null,
+//   selectedStation: null
+// };
+//
+// $( _ => {
+//     const root = $('.root');
+//     render(root);
+// });
